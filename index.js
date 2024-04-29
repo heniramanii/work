e5dda2062bcccb107fb58fb467f35f1e577075b1
heniramanii <167847745+heniramanii@users.noreply.github.com>
2024-04-29 15:28:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function  calculatesubtotal() {
+    var n1 = document.getElementById("on").value
+    var n2  = 29.99
+    var total = Number(n1)*Number(n2)
+    document.getElementById("total").innerHTML=total
+}
+
+function on()
+{
+    var one = document.getElementById("on")
+}
+
+
+function calculatesubtotal1() {
+    var m1 = document.getElementById("two").value
+    var m2 = 12.99
+    var result  = Number(m1)*Number(m2)
+    document.getElementById("result1").innerHTML = result
+}
+
+function two() 
+{
+    var two = document.getElementById("two")
+}
